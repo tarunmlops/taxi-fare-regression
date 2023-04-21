7f1d07e92df1efbe9e260798382a45c7aa4dbccb
tarunmlops <131292923+tarunmlops@users.noreply.github.com>
2023-04-21 13:14:16 +0100

    Update main.tf

--- infrastructure/modules/storage-account/main.tf
+++ infrastructure/modules/storage-account/main.tf
@@ -10,7 +10,7 @@ locals {
 }
 
 resource "azurerm_storage_account" "st" {
-  name                     = "st${local.safe_prefix}${local.safe_postfix}${var.env}01"
+  name                     = "st1${local.safe_prefix}${local.safe_postfix}${var.env}01"
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
